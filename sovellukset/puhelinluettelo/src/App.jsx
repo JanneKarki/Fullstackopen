883b89d80c44fc36989629daab1d5ecc59e2db33
Janne Kärki <janne.karki@helsinki.fi>
2025-05-25 13:42:48 +0300

    Tehtävät 2.12, 2.13: puhelinluettelo step7, step8 tehty

--- sovellukset/puhelinluettelo/src/App.jsx
+++ sovellukset/puhelinluettelo/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 
 import Filter from '../components/Filter'
 import PersonForm from '../components/PersonForm'
 import Persons from '../components/Persons'
+import personService from '../services/persons'
 
 
 const App = () => {
@@ -13,10 +13,10 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -46,9 +46,13 @@ const App = () => {
       number: newNumber
     }
 
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+    personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
   const personsToShow = persons.filter(person =>
