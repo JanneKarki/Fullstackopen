41880861ca4007a04127fcbbb68fe8f76519eefe
Janne Kärki <janne.karki@helsinki.fi>
2025-05-25 16:08:34 +0300

    Tehtävä 2.19*: maiden tiedot, step2 tehty

--- sovellukset/countries/src/App.jsx
+++ sovellukset/countries/src/App.jsx
@@ -1,9 +1,28 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const CountryDetails = ({ country }) => (
+  <div>
+    <h1>{country.name.common}</h1>
+    <p>Capital {country.capital}</p>
+    <p>Area {country.area}</p>
+
+    <h3>Languages</h3>
+    <ul>
+      {Object.values(country.languages).map(lang =>
+        <li key={lang}>{lang}</li>
+      )}
+    </ul>
+
+    <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="150" />
+  </div>
+)
+
+
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
+  const [selectedCountry, setSelectedCountry] = useState(null)
 
   useEffect(() => {
     axios
@@ -15,6 +34,11 @@ const App = () => {
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
+    setSelectedCountry(null)
+  }
+
+  const handleShowCountry = (country) => {
+    setSelectedCountry(country)
   }
 
   const filtered = countries.filter(country =>
@@ -26,12 +50,16 @@ const App = () => {
       <div>
         find countries <input value={filter} onChange={handleFilterChange} />
       </div>
-      <Results countries={filtered} />
+      <Results countries={filtered} selected={selectedCountry} onShow={handleShowCountry} />
     </div>
   )
 }
 
-const Results = ({ countries }) => {
+const Results = ({ countries, selected, onShow }) => {
+  if (selected) {
+    return <CountryDetails country={selected} />
+  }
+
   if (countries.length > 10) {
     return <p>Too many matches, specify another filter</p>
   }
@@ -39,29 +67,18 @@ const Results = ({ countries }) => {
   if (countries.length > 1) {
     return (
       <ul>
-        {countries.map(country =>
-          <li key={country.cca3}>{country.name.common}</li>
-        )}
+        {countries.map(country => (
+          <li key={country.cca3}>
+            {country.name.common}
+            <button onClick={() => onShow(country)}>Show</button>
+          </li>
+        ))}
       </ul>
     )
   }
 
   if (countries.length === 1) {
-    const country = countries[0]
-    return (
-      <div>
-        <h1>{country.name.common}</h1>
-        <p>Capital {country.capital}</p>
-        <p>Area {country.area}</p>
-        <h3>Languages</h3>
-        <ul>
-          {Object.values(country.languages).map(lang =>
-            <li key={lang}>{lang}</li>
-          )}
-        </ul>
-        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="150" />
-      </div>
-    )
+    return <CountryDetails country={countries[0]} />
   }
 
   return <p>No matches</p>
