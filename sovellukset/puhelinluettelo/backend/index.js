dfa876601b1eabb42e2d9a3aea30b2453b0dfd5d
Janne Kärki <janne.karki@helsinki.fi>
2025-05-27 20:54:54 +0300

    Tehtävä 3.4: puhelinluettelon backend step4 tehty

--- sovellukset/puhelinluettelo/backend/index.js
+++ sovellukset/puhelinluettelo/backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
   { id: 1, name: 'Arto Hell', number: '040-123456' },
   { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
   { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
@@ -26,6 +26,26 @@ app.get('/api/persons', (req, res) => {
 app.get('/', (req, res) => {
     res.send('This is phonebook backend. Go to api/persons or /info')
   })
+
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+  
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+});
+
 
 const PORT = 3002
 app.listen(PORT, () => {
