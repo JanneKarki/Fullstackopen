d4bfcf86deed49cb073407d57e5fda02c4976f8b
Janne Kärki <janne.karki@helsinki.fi>
2025-05-28 22:48:19 +0300

    Tehtävä 3.11 puhelinluettelo full stack tehty

--- sovellukset/puhelinluettelo/backend/index.js
+++ sovellukset/puhelinluettelo/backend/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan');
 const cors = require('cors')
+const path = require('path');
 
 app.use(cors())
 
 app.use(express.json())
 
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : '';
