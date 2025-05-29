36c86455a349aa69cf9422dc25ea85fbc7e22999
Janne Kärki <janne.karki@helsinki.fi>
2025-05-29 20:15:34 +0300

    Tehtävä 4.5*: apufunktioita ja yksikkötestejä, step3 tehty

--- sovellukset/blogilista/utils/list_helper.js
+++ sovellukset/blogilista/utils/list_helper.js
@@ -6,8 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+  
+    return blogs.reduce((fav, blog) => {
+      return blog.likes > fav.likes ? blog : fav
+    }, blogs[0])
+  }
+  
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
   
