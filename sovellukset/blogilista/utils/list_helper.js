d5de9d7c5daa2df21c8c7ad2ee5f0a74083c886f
Janne Kärki <janne.karki@helsinki.fi>
2025-05-29 20:46:38 +0300

    Tehtävä 4.6*: apufunktioita ja yksikkötestejä, step4 tehty

--- sovellukset/blogilista/utils/list_helper.js
+++ sovellukset/blogilista/utils/list_helper.js
@@ -18,9 +18,36 @@ const favoriteBlog = (blogs) => {
   }
   
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const authorCounts = {}
+
+  blogs.forEach((blog) => {
+    authorCounts[blog.author] = (authorCounts[blog.author] || 0) + 1
+  })
+
+  let maxAuthor = null
+  let maxBlogs = 0
+
+  for (const author in authorCounts) {
+    if (authorCounts[author] > maxBlogs) {
+      maxAuthor = author
+      maxBlogs = authorCounts[author]
+    }
+  }
+
+  return {
+    author: maxAuthor,
+    blogs: maxBlogs,
+  }
+}
+
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
   
