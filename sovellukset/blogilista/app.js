49ea5f0688b83441b5a808166e63819b546ca222
Janne Kärki <janne.karki@helsinki.fi>
2025-05-31 21:15:10 +0300

    Tehtävä 4.18: blogilistan laajennus, step6 tehty

--- sovellukset/blogilista/app.js
+++ sovellukset/blogilista/app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const { errorHandler } = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -19,5 +20,6 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use(errorHandler)
+app.use('/api/login', loginRouter)
 
 module.exports = app
