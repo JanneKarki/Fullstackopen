153fbe13f12b32662c20e9524011a18e28c95dbb
Janne Kärki <janne.karki@helsinki.fi>
2025-06-01 19:41:31 +0300

    Tehtävä 5.3: blogilistan frontend, step3 tehty

--- sovellukset/blogilista/frontend/src/App.jsx
+++ sovellukset/blogilista/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -39,6 +40,15 @@ const App = () => {
     setUser(null)
   }
 
+  const createBlog = async (blogObject) => {
+    try {
+      const newBlog = await blogService.create(blogObject)
+      setBlogs(blogs.concat(newBlog))
+    } catch (error) {
+      console.error('failed to create blog', error)
+    }
+  }
+  
   if (user === null) {
     return <LoginForm onLogin={handleLogin} />
   }
@@ -48,6 +58,7 @@ const App = () => {
       <h2>blogs</h2>
       <p>{user.name} logged in <button onClick={handleLogout}>logout</button>
       </p>
+      <BlogForm createBlog={createBlog} />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
