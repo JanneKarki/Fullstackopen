4fcabcfa0ec5e132ac1d9c276389f340e007a90f
Janne Kärki <janne.karki@helsinki.fi>
2025-06-01 20:48:07 +0300

    Tehtävät 5.7, 5.8 blogilistan frontend, step7, step 8 tehty

--- sovellukset/blogilista/frontend/src/components/Blog.jsx
+++ sovellukset/blogilista/frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  if (!visible) {
+    return (
+      <div className="blog">
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="blog">
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>hide</button>
+      </div>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      <div>{blog.user?.name}</div>
+    </div>
+  )
+}
 
 export default Blog
