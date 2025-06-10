b1be0277de04f6d89758cd6cfeb3287e996cc006
Janne Kärki <janne.karki@helsinki.fi>
2025-06-10 20:10:36 +0300

    Tehtävä 5.17: blogilistan end to end ‑testit, step1 tehty

---
+++ sovellukset/blogilista/e2e/tests/blog_app.spec.js
@@ -0,0 +1,22 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'testaaja',
+        username: 'meitsi2',
+        password: 'salasana'
+      }
+    })
+
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByTestId('username')).toBeVisible()
+    await expect(page.getByTestId('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+})
